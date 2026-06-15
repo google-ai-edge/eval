37ba83599f9425a4970aab6fab0576a4213ab033
Jingxiao Zheng <jingxiaozheng@google.com>
2026-06-15 09:15:44 -0700

    Implement task groups for custom evaluation.
    PiperOrigin-RevId: 932498540

--- model_eval/custom_tasks/__init__.py
+++ model_eval/custom_tasks/__init__.py
@@ -15,6 +15,9 @@
 """Export custom task layer modules."""
 
 from model_eval.custom_tasks import base
+from model_eval.custom_tasks import groups
+from model_eval.custom_tasks import loaders
+from model_eval.custom_tasks import metrics
 from model_eval.custom_tasks import registry
 
 OpenAIMessages = base.OpenAIMessages
@@ -22,9 +25,49 @@ CustomTask = base.CustomTask
 DatasetRow = base.DatasetRow
 TaskRegistry = registry.TaskRegistry
 
+# Metrics re-exports.
+register_metric = metrics.register_metric
+get_metric = metrics.get_metric
+list_metrics = metrics.list_metrics
+clear_metrics = metrics.clear_metrics
+compose = metrics.compose
+
+# Normalizer re-exports.
+register_normalizer = metrics.register_normalizer
+get_normalizer = metrics.get_normalizer
+list_normalizers = metrics.list_normalizers
+clear_normalizers = metrics.clear_normalizers
+
+# Loaders re-exports.
+register_loader = loaders.register_loader
+get_loader = loaders.get_loader
+list_loaders = loaders.list_loaders
+clear_loaders = loaders.clear_loaders
+
+# Groups re-exports.
+is_group = groups.is_group
+subtasks_of = groups.subtasks_of
+list_groups = groups.list_groups
+
 __all__ = [
     "OpenAIMessages",
     "CustomTask",
     "DatasetRow",
     "TaskRegistry",
+    "register_metric",
+    "get_metric",
+    "list_metrics",
+    "clear_metrics",
+    "compose",
+    "register_normalizer",
+    "get_normalizer",
+    "list_normalizers",
+    "clear_normalizers",
+    "register_loader",
+    "get_loader",
+    "list_loaders",
+    "clear_loaders",
+    "is_group",
+    "subtasks_of",
+    "list_groups",
 ]
